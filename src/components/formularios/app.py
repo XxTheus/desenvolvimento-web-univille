9e35ffb2444ef40767d02937ab38e6fd83b86418
Matheus de Araújo <matheusdearaujo1209@gmail.com>
2023-05-08 20:31:21 -0300

    Flask

--- src/components/formularios/app.py
+++ src/components/formularios/app.py
@@ -5,4 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     dados = request.args
-    return f"<p>Nome {dados['nome']}</p>"
+    if dados['nome'] == "Matheus":
+        return f"<p>{dados['nome']} Válido</p>"
+    else:
+        return f"<p>{dados['nome']} Inválido</p>"
+    #return f"<p>Nome {dados['nome']}</p>"
