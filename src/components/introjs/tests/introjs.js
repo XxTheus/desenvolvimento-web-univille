c1dd1403fe9f420d2d3fe8b340f1da5e70fe34a0
Matheus de Araújo <matheusdearaujo1209@gmail.com>
2023-05-22 20:29:12 -0300

    Introdução ao JS

---
+++ src/components/introjs/tests/introjs.js
@@ -0,0 +1,23 @@
+// alert("Hello World")
+
+/*  
+var resposta = confirm("Você está certo disso?")
+alert("Sua resposta foi: " + resposta)
+*/
+
+
+var nome = prompt("Digite seu nome: ")
+
+if (nome == "") {
+    alert("Usuário desconhecido")
+}
+else {
+    alert("Seu nome é: " + nome)
+}
+
+window.document.write("Opa opa opa opa opa")
+window.document.title = "Título da página"
+
+function meuCodigo(){
+    document.write("Meu código")
+}
