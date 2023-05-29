ab2f67302b5733af70cb3bc9be4dcd7f59979dd0
Matheus de Araújo <matheusdearaujo1209@gmail.com>
2023-05-29 20:24:27 -0300

    Adicionando novos botões, centralizando e atribuindo funções

--- src/components/introjs/calculadora/calculadora.js
+++ src/components/introjs/calculadora/calculadora.js
@@ -4,3 +4,15 @@ function botaoClicado(numero){
     console.log(numero);
     refDisplay.value += numero
 }
+
+function limparTudo(){
+    refDisplay.value = ''
+}
+
+function limparUm(){
+    refDisplay.value = refDisplay.value.substring(0, refDisplay.value.length - 1)
+}
+
+function resultado(){
+    refDisplay.value = eval(refDisplay.value)
+}
