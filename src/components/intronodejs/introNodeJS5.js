9d5cf059663ca7478b18a2a40eb0ba3fa1594477
Matheus de Araújo <matheusdearaujo1209@gmail.com>
2023-06-12 20:37:46 -0300

    Promise

---
+++ src/components/intronodejs/introNodeJS5.js
@@ -0,0 +1,17 @@
+function soma(a, b){
+    return new Promise(function(resolve, reject){
+        setTimeout(function() {
+            resolve(a + b)
+        }, 2000);
+    })
+}
+
+/* const resultado = soma(2, 2).then(function(result){
+    console.log(result)
+})
+console.log(resultado) */
+
+(async function(){
+    const result = await soma(2, 2)
+    console.log(result)
+})()
